79b9cf70ad8eb2c4fcdc6055d18ba2a21913bee0
Aaron-Celia <aaron.r.celia@gmail.com>
2023-12-17 16:01:27 -0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,101 @@
-import Image from 'next/image'
+'use client';
+import { Alert, Button, Container, Slider, Stack, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
+  const [password, setPassword] = useState('password');
+  const [desiredLength, setDesiredLength] = useState(12);
+  const [generated, setGenerated] = useState(false);
+  const [errorMsg, setErrorMessage] = useState('');
+  const [copied, setCopied] = useState(false);
+
+  let copiedTimeout = 3000; // milliseconds to show alert that they copied the message
+  let generatedTimeout = 2000; // milliseconds to show different button
+
+  const generate = () => {
+    const specialChars = '!@#$%^&*()';
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const nums = '1234567890';
+
+    
+    const getRandom = (string) => {
+      const randNum = Math.floor(Math.random() * string.length);
+      return string[randNum]
+    };
+    
+    let password = [];
+    
+    const charArrAll = [specialChars, alphabet, nums];
+
+    while(password.length < desiredLength) {
+      // generate random number between 0 and 2 (2 inclusive)
+      let i = Math.floor(Math.random() * charArrAll.length);
+      // pick one of the three strings
+      let str = charArrAll[i];
+      // get random character from that string based on it's length
+      let randomChar = getRandom(str);
+      console.log(randomChar);
+      // push it to password array
+      password.push(randomChar)
+    }
+
+    let result = password.join('');
+    
+    if(result.length === desiredLength) {
+      setGenerated(true);
+      setPassword(result);
+      setTimeout(() => {
+        setGenerated(false)
+      }, generatedTimeout)
+    } else {
+      setPassword('');
+      setErrorMessage('Something went wrong.');
+    }
+  }
+
+
+  let centerSx = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '90%'
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Container className='h-screen text-center' sx={centerSx}>
+      <Typography className='my-10' variant='h4'>Password-Generating Algorithm</Typography>
+      <Stack sx={centerSx}>
+        <Typography>Password Length</Typography>
+        <Typography>{desiredLength}</Typography>
+        <Slider value={desiredLength} onChange={(e) => setDesiredLength(e.target.value)} min={8} max={20} />
+      </Stack>
+      <Stack sx={centerSx}>
+        <code className="my-10 bg-green-800 p-5 rounded-xl">
+          <>{password.length ? password : errorMsg}</>
+        </code>
+        <Button variant="outlined" className="my-10" color={generated ? "success" : "primary"} onClick={() => generate()}>Generate</Button>
+        <Button variant="outlined" className="my-10" color={copied ? "success" : "primary"} onClick={() => {
+          try{
+            window.navigator.clipboard.writeText(`${password}`);
+            setCopied(true)
+            setTimeout(() => {
+              setCopied(false)
+            }, copiedTimeout);
+          } catch(err) {
+            setErrorMessage(err);
+          }
+        }}>Copy Password</Button>
+        {generated &&
+          <Alert className='my-5' severity={generated ? 'success' : 'error'}>{generated ? 'Password Generated!' : errorMsg}</Alert>
+        }
+        {errorMsg &&
+          <Alert className='my-5' severity="error">{errorMsg}</Alert>
+        }
+        {copied &&
+          <Alert className='my-5' severity="success">Copied!</Alert>
+        }
+      </Stack>
+    </Container>
   )
 }
